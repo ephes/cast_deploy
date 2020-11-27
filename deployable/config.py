e555a1dca1759c9b35fc5d1b5feb4909f924f507
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-11-27 07:10:10 +0100

    env variables with .env file

---
+++ deployable/config.py
@@ -0,0 +1,11 @@
+from pydantic import Field
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "Makes Services Deployable"
+    admin_email: str = "jochen-deployable@wersdoerfer.de"
+    database_url: str = Field(..., env="DATABASE_URL")
+
+    class Config:
+        env_file = ".env"
