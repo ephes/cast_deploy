7ec9d7eb4b170e29cc593cc48250544f1d8872da
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-12-14 06:58:56 +0100

    added async api test

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,6 +1,19 @@
+import pytest
+
 from ..config import settings
+
+from httpx import AsyncClient
+
 
 def test_read_hello(client):
     response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.json() == {"message": "hello from fastapi"}
+
+
+@pytest.mark.asyncio
+async def test_async_read_hello(fastapi_app):
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        response = await ac.get("/hello")
     assert response.status_code == 200
     assert response.json() == {"message": "hello from fastapi"}
