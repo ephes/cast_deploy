c82b96e5df3b025a5ea0f8511556bd841b531ff3
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-01-03 16:43:09 +0100

    vue router basically working

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Home Page</h1>
+  <p>Message from api:</p>
+  {{ state.message }}
+</template>
+<script>
+import { reactive } from "vue";
+
+export default {
+  name: 'Home',
+  setup() {
+    const state = reactive({
+      message: "no message yet..",
+    });
+
+    return {
+      state,
+    };
+  },
+
+  mounted() {
+    console.log("home mounted..");
+    this.axios
+      .get("http://localhost:8000/hello")
+      .then((response) => {
+        console.log(response)
+        this.state.message = response.data.message;
+      });
+  },
+};
+</script>
