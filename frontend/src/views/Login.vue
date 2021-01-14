57cd64045072699ace78493dcca369fae9c0a57b
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-01-14 21:27:33 +0100

    ignore pycharm config

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Login View</h1>
+  <p>Logged In state: {{ state.isAuthenticated }}</p>
+  <button @click="login">
+    Log in..
+  </button>
+</template>
+<script>
+import { reactive } from "vue";
+import { useRouter } from 'vue-router'
+
+import api from "../api"
+
+export default {
+  name: 'Home',
+  setup() {
+    const router = useRouter()
+
+    function login() {
+      api.isAuthenticated = true;
+      router.push('/')
+    }
+
+    return {
+      state: {
+        isAuthenticated: api.isAuthenticated,
+      },
+      login: login,
+    };
+  },
+};
+</script>
